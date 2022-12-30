c59fd90d2c7507283be94a636d87d726e1afd01d
John <jhaywa02@gmail.com>
2022-12-30 16:59:29 -0600

    Added folder and separate files for each world/Fixed spelling

---
+++ worlds/swamps.py
@@ -0,0 +1,5 @@
+
+def swamps():
+    input()
+    print(
+        'Your first feeling is your boots filling with water and grime. next you feel your nose filling with the humid air. You notice parts where the plants seem to be vacant. its a trail walked by whoever inhabits this world. ')
---
+++ worlds/world.py
@@ -0,0 +1,8 @@
+import random
+
+
+def randomName():
+    nameList = ["Zofia", "Yusuf", "Alejandro", "Elisabeth", "Rex", "Brady", "Montell", "Shaan", "Safwan", "Rachael",
+                "Nel", "Mekhi", "Karol", "Maddy", "Arvin", "Maisie", "Osian", "Darrel", "Tristan", "Devan", "Nicky",
+                "Joe", "Francis", "John"]
+    return random.choice(nameList)
